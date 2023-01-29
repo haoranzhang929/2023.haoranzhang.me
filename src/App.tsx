70fc9274e3ef9d59f0e1ae97ad077340a47157bb
Haoran Zhang <haoranzhang@icloud.com>
2023-01-29 00:21:46 +0000

    Wrap app with ModeContext provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
+import { ModeProvider } from "./context";
 import Router from "./router";
 
 const App = () => {
   return (
-    <div className="h-screen w-screen bg-neutral-200 text-zinc-800 dark:bg-neutral-900 dark:text-white">
-      <Header />
-      <Router />
-    </div>
+    <ModeProvider>
+      <div className="h-screen w-screen bg-neutral-200 text-zinc-800 dark:bg-neutral-900 dark:text-white">
+        <Header />
+        <Router />
+      </div>
+    </ModeProvider>
   );
 };
 
